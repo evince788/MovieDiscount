c9c48aca9fc602850d21acaf9c7b9fe597b69fe4
Elliott Vince <elliott.vince788@sccdsb.net>
2020-09-25 13:36:24 -0400

    Pushed to master

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -1,25 +1,41 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer: Elliott Vince
+ * Date: September 25, 2020
+ * Purpose: Calculates the price of a movie ticket 
+ * 			based on the user's age and if they have a coupon.
  */
 
 import java.util.Scanner;
 
 public class MovieDiscount {
+	
     public static void main (String [] args) {
         // Create the input scanner
         Scanner input = new Scanner(System.in);
 
-        // Ask the user for the side lengths
+        // Ask the user for their age
         System.err.print("How old are you? ");
         int age = input.nextInt();
 
         // Ask the user if they have a coupon
         System.err.print("Do you have a coupon? [true/false] ");
-        // TODO: Read in a true/false value
+        boolean hasCoupon = input.nextBoolean();
 
+        // Creates the price variable and assigns the default cost of the ticket to it.
+        double price = 14.50;
+        
         // Print the cost of a movie ticket
-        // TODO: Figure out the cost and print it
+        if (age < 13 || age >= 65) {
+        	price -= 3.0;
+        	
+        	if (hasCoupon) {
+        		price -= 2.0;
+        	}
+        }
+        else if (hasCoupon) {
+        	price -= 2.0;
+        }
+        
+        System.out.println("Your ticket costs $" + price + ".");
     }
 }
